cf7eb84623149f0da99b3260bd995b2436c49ee2
Ivan MArkovski <sagitariusim@gmail.com>
2023-12-22 16:15:03 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+# Make the creating of our model a little easier
+from keras.src.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Activation
+from tensorflow.keras import Sequential
+import tensorflow as tf
+
+train_dir = "pizza_steak/train/"
+test_dir = "pizza_steak/test/"
+tf.random.set_seed(42)
+# Create ImageDataGenerator training instance with data augmentation
+train_datagen_augmented = ImageDataGenerator(rescale=1 / 255.,
+                                             rotation_range=20,  # rotate the image slightly between 0 and 20 degrees
+                                             # (note: this is an int not a float)
+                                             shear_range=0.2,  # shear the image
+                                             zoom_range=0.2,  # zoom into the image
+                                             width_shift_range=0.2,  # shift the image width ways
+                                             height_shift_range=0.2,  # shift the image height ways
+                                             horizontal_flip=True)  # flip the image on the horizontal axis
+
+# Create ImageDataGenerator training instance without data augmentation
+train_datagen = ImageDataGenerator(rescale=1 / 255.)
+
+# Create ImageDataGenerator test instance without data augmentation
+test_datagen = ImageDataGenerator(rescale=1 / 255.)
+
+# Import data and augment it from training directory
+print("Augmented training images:")
+train_data_augmented = train_datagen_augmented.flow_from_directory(train_dir,
+                                                                   target_size=(224, 224),
+                                                                   batch_size=32,
+                                                                   class_mode='binary',
+                                                                   shuffle=True)
+
+# Create non-augmented data batches
+print("Non-augmented training images:")
+train_data = train_datagen.flow_from_directory(train_dir,
+                                               target_size=(224, 224),
+                                               batch_size=32,
+                                               class_mode='binary',
+                                               shuffle=True)  # Don't shuffle for demonstration purposes
+
+print("Unchanged test images:")
+test_data = test_datagen.flow_from_directory(test_dir,
+                                             target_size=(224, 224),
+                                             batch_size=32,
+                                             class_mode='binary')
+
+# Create the model (same as model_5 and model_6)
+model_7 = Sequential([
+  Conv2D(10, 3, activation='relu', input_shape=(224, 224, 3)),
+  MaxPool2D(),
+  Conv2D(10, 3, activation='relu'),
+  MaxPool2D(),
+  Conv2D(10, 3, activation='relu'),
+  MaxPool2D(),
+  Flatten(),
+  Dense(1, activation='sigmoid')
+])
+
+# Compile the model
+model_7.compile(loss='binary_crossentropy',
+                optimizer=Adam(),
+                metrics=['accuracy'])
+
+# Fit the model
+history_7 = model_7.fit(train_data_augmented, # now the augmented data is shuffled
+                        epochs=5,
+                        steps_per_epoch=len(train_data_augmented),
+                        validation_data=test_data,
+                        validation_steps=len(test_data))
