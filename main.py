6e12d8b7fab537f5ba62c1d12c8aa0dae334d51f
Ivan Markovski <sagitariusim@gmail.com>
2024-03-10 17:55:23 +0100

    update

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # Make the creating of our model a little easier
+import pathlib
+
 from keras.src.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Activation
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D
 from tensorflow.keras import Sequential
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Plot the validation and training data separately
@@ -34,10 +37,34 @@ def plot_loss_curves(history):
     plt.title('Accuracy')
     plt.xlabel('Epochs')
     plt.legend()
+    plt.show()
 
 
+def load_and_prep_image(filename: str, img_shape=224):
+    img = tf.io.read_file(filename)
+    img = tf.image.decode_jpeg(img)
+    img = tf.image.resize(img, size=[img_shape, img_shape])
+    img = img / 255.
+    return img
+
+
+def pred_and_plot(model, filename: str, class_name):
+    img = load_and_prep_image(filename)
+    pred = model.predict(tf.expand_dims(img, axis=0))
+    pred_class = class_name[int(tf.round(pred))]
+    plt.imshow(img)
+    plt.title(f"Prediction {pred_class}")
+    plt.axis(False)
+    plt.show()
+
+
+print(tf.__version__)
+
+# Set up the train and test directories
 train_dir = "pizza_steak/train/"
 test_dir = "pizza_steak/test/"
+data_dir = pathlib.Path(train_dir)
+class_names = np.array(sorted([item.name for item in data_dir.glob('*')]))
 tf.random.set_seed(42)
 # Create ImageDataGenerator training instance with data augmentation
 train_datagen_augmented = ImageDataGenerator(rescale=1 / 255.,
@@ -100,3 +127,8 @@ history_7 = model_7.fit(train_data_augmented,  # now the augmented data is shuff
                         steps_per_epoch=len(train_data_augmented),
                         validation_data=test_data,
                         validation_steps=len(test_data))
+
+steak = load_and_prep_image("03-steak.jpeg")
+pred = model_7.predict(tf.expand_dims(steak, axis=0))
+pred_and_plot(model_7, "03-steak.jpeg", class_names)
+# plot_loss_curves(history_7)
